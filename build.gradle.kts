ddc52389fc6e56ad41bb513093895e2e6c0db765
Shanty <shanty@vault.app>
2026-07-15 04:08:51 +0000

    Fix KSP version to 2.2.21-2.0.5 (matching Kotlin 2.2.21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.devtools.ksp") version "2.2.21-1.0.30" apply false
+    id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
 }
